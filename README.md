cf7b72c7210e28000b033d2425f72be22cff8f9d
admbarry <admbarry@gmail.com>
2014-08-14 19:16:57 +0200

    Epic-style commit - a lot has been optimized; compiled folder has been added with grunt watch task compiling the files. Hence you can now choose to have either the source or the compiled version; multiple test-cases added and some have been eliminated (housekeeping); more gutter options etc.

---
+++ README.md
@@ -0,0 +1,6 @@
+Flexbox Grid
+============
+
+A comprehensive CSS3 Flexible Box (Flexbox) grid system.
+
+Visit: http://adambarry.github.io/Flexboss/ to view the test page.
---
+++ gruntfile.js
@@ -0,0 +1,103 @@
+/*******************************************************************************
+Use Grunt to process JavaScript and LESS files
+
+Inspiration:
+http://justinklemm.com/grunt-watch-livereload-javascript-less-sass-compilation/
+
+
+How to use (if you already have nodejs and Grunt installed, skip to step 3):
+
+1)  Install Node.js (with Node Package Manager/NPM) from
+    http://nodejs.org/download/
+
+2)  Install Grunt (globally):
+    sudo npm install -g grunt-cli
+
+3)  Open a terminal window, "cd" into the project folder and type:
+    npm install
+
+    This will install Node.js in the project folder; perform an initial
+    compilation of the files that Grunt are monitoring; and start a Grunt watch
+    task (which you can cancel by typing "ctrl c").
+
+4)  If you cancel the Grunt watch task, you can restart it by typing:
+    grunt watch
+
+*******************************************************************************/
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        less: {
+            style: {
+                files: {
+                    "compiled/css/grid.css": "source/**/*.less" //Process the manifest "css/grid.less" and output to "compiled/css/grid.css"
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: [
+                    '**/*.less',
+                    '**/*.css'
+                ],
+                tasks: ['less:style'],
+                options: {
+                    livereload: true
+                }
+            },
+            html: {
+                files: [
+                    '**/*.html'
+                ],
+                options: {
+                    livereload: true
+                }
+            },
+            livereload: {
+                options: { livereload: true },
+                files: [
+                    '**/*.html'
+                ]
+            }
+        }
+    });
+
+
+////////////////////////////////////////////////////////////////////////////////
+//: Register tasks
+////////////////////////////////////////////////////////////////////////////////
+
+    // Init
+    // Execute an initial compilation of the JavaScript and LESS/CSS files and
+    // start a Grunt watch task.
+    //
+    // Used when executing an "npm install" e.g. from the terminal
+
+    grunt.registerTask('init', [
+        //'concat',
+        //'uglify',
+        'less',
+        'watch'
+    ]);
+
+
+    // Watch
+    // Compile the JavaScript and LESS/CSS files when saving the files and
+    // livereload connected browsers
+
+    grunt.registerTask('watch', [
+        'watch'
+    ]);
+
+
+////////////////////////////////////////////////////////////////////////////////
+//: Load dependencies
+////////////////////////////////////////////////////////////////////////////////
+
+    //grunt.loadNpmTasks('grunt-contrib-concat');
+    //grunt.loadNpmTasks('grunt-contrib-jshint');
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
